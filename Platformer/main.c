b985495dd802d0abd41ad72d3f7ecc3e6ad00fa2
Keysi Demon <kyh3im@gmail.com>
2013-02-09 17:49:34 +0200

    Start

--- Platformer/main.c
+++ Platformer/main.c
@@ -1,17 +1,7 @@
-//
-//  main.c
-//  Platformer
-//
-//  Created by Keysi Demon on 09.02.13.
-//  Copyright 2013 __MyCompanyName__. All rights reserved.
-//
-
 #include <stdio.h>
 
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
     printf("Hello, World!\n");
     return 0;
 }
